8e8cbaca8ea4e3015bf0ebfa521930e8c0755de6
Winter H. <a2405392127a@gmail.com>
2023-10-01 15:50:43 -0700

    witchhazel.nvim

---
+++ witchhazel.nvim/witchhazel/lua/witchhazel/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+local theme = require('witchhazel.theme')
+
+M.setup = function()
+  vim.cmd('hi clear')
+
+  vim.o.background = 'dark'
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'witchhazel'
+
+  theme.set_highlights()
+end
+
+return M
