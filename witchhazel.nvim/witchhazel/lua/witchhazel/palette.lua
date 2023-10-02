8c80c918fda7332e0a0d1599d7e494bff3b7b4fe
Winter <102400503+exoess@users.noreply.github.com>
2023-10-02 08:55:37 -0700

    witchhazel.nvim (#42)

---
+++ witchhazel.nvim/witchhazel/lua/witchhazel/palette.lua
@@ -0,0 +1,19 @@
+local colors = {
+  lightgrey = "#b0bec5",
+  linen = "#f8f8f2",
+  mint = "#c2ffdf",
+  pink = "#ff857f",
+  brick = "#f92672",
+  clay = "#a8757b",
+  rouge = "#960050",
+  turquoise = "#1bc5e0",
+  sunflower = "#fff352",
+  lilac = "#ceb1ff",
+  darklilac = "#c5a3ff",
+  amethyst = "#716799",
+  purps = "#433e56",
+  shadow = "#3b364e",
+  midnight = "#1e0010",
+}
+
+return colors
